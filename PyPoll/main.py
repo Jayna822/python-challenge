8c884d1c964b7c29f9e95c94cfcff425ee026921
Jayna Kehres <jayna822@gmail.com>
2019-03-09 21:44:37 -0600

    Added some comments to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,12 +10,14 @@ with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
+    # In the video, I created the below variable, but we actually don't need it
+    # We could just sum up the values in the dictionary after the for-loop
+    # In any case, it's up to you. I wouldn't say this makes the code less efficient,
+    #   maintainable, or readable. So your call.
     total_votes = 0
     candidates = {}
 
     for row in csvreader:
-        voter_id = row[0]
-        county = row[1]
         candidate = row[2]
 
         total_votes += 1
@@ -25,17 +27,19 @@ with open(csvpath, newline="") as csvfile:
         else:
             candidates[candidate] = 1
 
+number_of_dashes = 25
+print('Election Results')
+print('-' * number_of_dashes)
+print('Total Votes: %d'%total_votes)
+print('-' * number_of_dashes)
+
 highest_number_of_votes = 0
 winner = None
-print('Election Results')
-print('-'*20)
-print('Total Votes: %d'%total_votes)
-print('-'*20)
 for candidate, votes in candidates.items():
     percentage = votes / total_votes * 100
     print('%s: %.2f%% (%d)'%(candidate, percentage, votes))
     if votes > highest_number_of_votes:
         winner = candidate
         highest_number_of_votes = votes
-print('-'*20)
+print('-' * number_of_dashes)
 print('Winner: %s'%winner)
